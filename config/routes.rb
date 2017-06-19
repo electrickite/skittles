963d9ae2efcf25597f78a8679687fb1dfd28e12c
Corey Hinshaw <hinshaw.25@osu.edu>
2017-06-19 00:15:21 -0400

    Run Devise generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   resources :moves
   resources :games
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "games#index"
 end

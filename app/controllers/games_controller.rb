897bed427ec0eb75db9291e4452f67517823ce3a
Corey Hinshaw <hinshaw.25@osu.edu>
2017-08-12 22:16:08 -0400

    Prevent editing of game completion date and result

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -71,7 +71,6 @@ class GamesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def game_params
-      params.require(:game).permit(:name, :owner_id, :result, :completed_at,
-        players_attributes: [:id, :color, :user_id ])
+      params.require(:game).permit(:name, :owner_id, players_attributes: [:id, :color, :user_id ])
     end
 end
